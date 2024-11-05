c113ba8f11cbefa60b54c87fa166f2e115a78782
DmitryTaushev <123morfix@gmail.com>
2024-11-05 23:23:40 +0500

    Создали функции в utils.py, которые проверяют данные через патерны

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+def my_idea():
+    name = "Имя"
+    surname = "Фамилия"
+    phone = "Телефон"
+    mail = "Почта"
+    return name,surname,phone,mail
+
 def get_reg_data():
     import re
     reg_name = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
@@ -9,4 +16,22 @@ def get_reg_data():
     my_reg_mail = re.compile(r'[a-zA-z0-9]+@yandex\.ru$')
 
     return reg_name,reg_surname,my_reg_phone,my_reg_mail
+
+
+def reg_check(user_data, reg_pattern, users_list, data_to_check = None):
+    import re
+    match_data = bool(re.match(reg_pattern, user_data))
+    return match_data
+
+def check_unique_data(user_data,data_to_check):
+    if user_data in data_to_check:
+        return True
+
+
+
+
+
+
+
+
 
